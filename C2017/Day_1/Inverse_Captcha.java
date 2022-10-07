1e7bd7ca93227384f6441cdf5996c3c2208a153a
GiimPy <GiimPy159@gmail.com>
2022-10-07 14:41:03 +0200

    moved code

---
+++ C2017/Day_1/Inverse_Captcha.java
@@ -0,0 +1,47 @@
+package VSCode_Exercises.AdventOfCode.C2017.Day_1;
+
+import VSCode_Exercises.AdventOfCode.Conveniencer;
+
+public class Inverse_Captcha {
+    private static final String DIRECTORY = "VSCode_Exercises/AdventOfCode/C2017/Day_1/input.txt";
+
+    public static void main(String[] args) {
+        String input = Conveniencer.getInput(DIRECTORY);
+         //Exercise 1
+        System.out.println(getCaptchaAtNextIndex(input));
+         //Exercise 2
+         System.out.println(getCaptchaAtHalfwayCircle(input));
+
+    }
+
+    private static int getCaptchaAtHalfwayCircle(String input) {
+        if (input.charAt(input.length()-1) == '\n')
+            input = input.substring(0, input.length()-1);
+
+        int sum = 0;
+        for (int i = 0; i<input.length(); i++) {
+            int adder = Integer.parseInt(String.valueOf(input.charAt(i)));
+            if (input.charAt(i) == input.charAt((i+(input.length()/2))%input.length())) {
+                sum += adder;
+            }
+        }
+        return sum;
+    }
+
+    private static int getCaptchaAtNextIndex(String input) {
+        //remove last char if it's \n
+        if (input.charAt(input.length()-1) == '\n')
+            input = input.substring(0, input.length()-1);
+
+        int sum = 0;
+        for (int i = 0; i<input.length(); i++) {
+            int adder = Integer.parseInt(String.valueOf(input.charAt(i)));
+            if (i != input.length()-1) {
+                if (input.charAt(i) == input.charAt(i+1))
+                    sum += adder;
+            } else if (input.charAt(i) == input.charAt(0)) 
+                sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+        }
+        return sum;
+    }
+}
---
+++ Conveniencer.java
@@ -0,0 +1,51 @@
+package VSCode_Exercises.AdventOfCode;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Conveniencer {
+    public static String getInput(String dir) {
+        String input = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(dir));
+            while (br.ready()){
+                input += br.readLine() + "\n";
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return("");
+        }
+        return input;
+    }
+
+    public static ArrayList<String> convertTextToLines(String text) {
+        ArrayList<String> lines = new ArrayList<>();
+        //Exception if no breakLine
+        if (text.indexOf("\n") == -1) {
+            lines.add(text);
+            return lines;
+        }
+        int breakLineIndex = 0;
+        int nextBreakLineIndex = 0;
+        while (true) {
+            nextBreakLineIndex = text.indexOf("\n", breakLineIndex);
+            if (nextBreakLineIndex == -1) break;
+            lines.add(text.substring(breakLineIndex, nextBreakLineIndex));
+            breakLineIndex = nextBreakLineIndex+1;
+        }
+        return lines;
+    }
+
+    public static int stringToInt(String s){
+        try {
+            int i = Integer.parseInt(s);
+            return i;
+        } catch (NumberFormatException e) {
+            System.out.println("Entered String can not be parsed to an int value, returned int = 0");
+            return 0;
+        }
+    }
+}
